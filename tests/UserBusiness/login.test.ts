2ef5a65936328ce6aa02209566105cb207f2609f
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-03-01 11:39:23 -0300

    Update

--- tests/UserBusiness/login.test.ts
+++ tests/UserBusiness/login.test.ts
@@ -1,5 +1,7 @@
 import { UserBusiness } from "../../src/business/UserBusiness"
 import { LoginInputDTO } from "../../src/dtos/userDTO"
+import { BadRequestError } from "../../src/errors/BadRequestError"
+import { NotFoundError } from "../../src/errors/NotFoundError"
 import { HashManagerMock } from "../mocks/HashManagerMock"
 import { IdGeneratorMock } from "../mocks/IdGeneratorMock"
 import { TokenManagerMock } from "../mocks/TokenManagerMock"
@@ -31,5 +33,50 @@ describe("login", () => {
 
         const response = await userBusiness.login(input)
         expect(response.token).toBe("token-mock-admin")
+    })
+
+    test("email não é string", async () => {
+        expect.assertions(1);
+        
+        const input: LoginInputDTO = {
+            email: true,
+            password: "bananinha"
+        }
+
+        try {
+            await userBusiness.login(input);
+        } catch(error){
+            if (error instanceof BadRequestError){
+                expect(error.message).toBe("'email' deve ser string");
+            }
+        }
+    })
+
+    test("email não foi encontrado", () => {
+        const input: LoginInputDTO = {
+            email: "naoexisto@mail.com",
+            password: "bananinha"
+        }
+
+        expect(async () => {
+            await userBusiness.login(input)
+        }).rejects.toBeInstanceOf(NotFoundError);
+    })
+
+    test("password incorreto", async () => {
+        expect.assertions(1);
+
+        const input: LoginInputDTO = {
+            email: "admin@email.com",
+            password: "password"
+        }
+
+        try {
+            await userBusiness.login(input);
+        } catch (error) {
+            if (error instanceof BadRequestError){
+                expect(error.message).toBe("'password' incorreto");
+            }
+        }
     })
 })
--- tests/UserBusiness/signup.test.ts
+++ tests/UserBusiness/signup.test.ts
@@ -23,4 +23,34 @@ describe("signup", () => {
         const response = await userBusiness.signup(input)
         expect(response.token).toBe("token-mock-normal")
     })
+
+    test("name precisa ser string", async () => {
+        expect.assertions(1);
+
+        const input = {
+            email: "example@email.com",
+            password: "bananinha" 
+        } as any;
+
+        try {
+            await userBusiness.signup(input);
+        } catch(error) {
+            if (error instanceof Error){
+                expect(error.message).toBe("'name' deve ser string");
+            }
+        }
+    })
+
+    test("email não pode se repetir", () => {
+
+        const input: SignupInputDTO = {
+            name: "Normal Mock",
+            email: "normal@email.com",
+            password: "hash-bananinha"
+        }
+        
+        expect(async () => {
+            await userBusiness.signup(input);
+        }).rejects.toThrow("'email' já existe");
+    })
 })
